4d30bf53737e0736c7b0caea08ac24ee068ad386
justindal <25357531+justindal@users.noreply.github.com>
2026-01-31 21:02:21 -0500

    refactor: new package version

--- Formula/apple-docs-mcp-server.rb
+++ Formula/apple-docs-mcp-server.rb
@@ -1,8 +1,8 @@
 class AppleDocsMcpServer < Formula
   desc "MCP Server for Apple Developer Documentation"
   homepage "https://github.com/justindal/Apple-Docs-MCP"
-  url "https://github.com/justindal/Apple-Docs-MCP/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "383c21cf24df81a7d4b983805c0e2020474d3a9eb96bdfe397d34247293d0df2"
+  url "https://github.com/justindal/Apple-Docs-MCP/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "c1c6f4c26e7b72f76882de377ef3d5388505bcd4d829502fc4b54aa2f7ac69a3"
   license "MIT"
   
   depends_on :xcode => ["16.0", :build] 
